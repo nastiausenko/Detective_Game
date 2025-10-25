28295fde960f634dfd4b934116a760ffbaaecfe7
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2025-10-25 14:32:00 +0300

    fix font styling

--- core/src/main/java/com/gdx/game/ui/popup/StoryPopup.java
+++ core/src/main/java/com/gdx/game/ui/popup/StoryPopup.java
@@ -1,5 +1,6 @@
 package com.gdx.game.ui.popup;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -7,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Align;
 import com.gdx.game.DetectiveGame;
 import com.gdx.game.utils.Assets;
@@ -18,7 +18,10 @@ public class StoryPopup extends AbstractPopup {
     private final Label storyLabel;
     private final Image continueButton;
 
-    private final String fullText = "This is the prologue of the story. Your adventure begins now...";
+    private final String fullText = "This is the prologue of the story. Your adventure begins now... " +
+        "This is the prologue of the story. Your adventure begins now..." +
+        "This is the prologue of the story. Your adventure begins now..." +
+        "This is the prologue of the story. Your adventure begins now...";
     private final StringBuilder sb = new StringBuilder();
     private float charTimer = 0f;
     private final float charDelay = 0.05f;
@@ -31,20 +34,19 @@ public class StoryPopup extends AbstractPopup {
         super(stage);
         this.game = game;
 
-        // Картинка прологу
         storyTexture = new Texture(Assets.PROLOGUE);
         storyImage = new Image(storyTexture);
 
-
-        // Лейбл для тексту
-        Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
         BitmapFont font = new BitmapFont(Gdx.files.internal("fonts/8bold.fnt"));
         font.getData().lineHeight *= 1.5f;
-        font.setColor(Color.BLACK);
 
-        storyLabel = new Label("", skin);
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = font;
+        labelStyle.fontColor = new Color(154 / 255f, 109 / 255f, 69 / 255f, 1f);
+
+        storyLabel = new Label("", labelStyle);
         storyLabel.setWrap(true);
-        storyLabel.setAlignment(Align.topLeft);
+        storyLabel.setAlignment(Align.center);
 
         continueButton = game.getButtonFactory().createButton(
             Assets.CONTINUE_BUTTON, 60, 60,
@@ -71,18 +73,31 @@ public class StoryPopup extends AbstractPopup {
     }
 
     public void resize(float screenWidth, float screenHeight) {
+        storyImage.setWidth(screenWidth);
         background.setSize(screenWidth, screenHeight);
         resizeCentered(storyImage, storyTexture, screenWidth, screenHeight);
 
-        float btnWidth = storyImage.getWidth() * 0.55f;
-        float btnHeight = storyImage.getHeight() * 0.17f;
-        float paddingBottom = storyImage.getHeight() * 0.07f;
+        float btnWidth = storyImage.getWidth() * 0.75f;
+        float btnHeight = storyImage.getHeight() * 0.15f;
+        float paddingBottom = storyImage.getHeight() * 0.1f;
 
-        storyLabel.setWidth(storyImage.getWidth() * 0.8f);
+
+        storyLabel.setWidth(storyImage.getWidth() * 0.7f);
         storyLabel.setPosition(
-                storyImage.getX() + storyImage.getWidth() * 0.1f,
-                storyImage.getY() + storyImage.getHeight() * 0.6f
+                storyImage.getX() + storyImage.getWidth() * 0.15f,
+                storyImage.getY() + storyImage.getHeight() * 0.52f
         );
+
+        boolean isMobile = Gdx.app.getType() == Application.ApplicationType.iOS
+            || Gdx.app.getType() == Application.ApplicationType.Android;
+
+        float scale;
+        if (isMobile) {
+            scale = (screenHeight / 1100f) * Gdx.graphics.getDensity() * 0.5f;
+        } else {
+            scale = screenHeight / 800f;
+        }
+        storyLabel.getStyle().font.getData().setScale(scale);
 
         continueButton.setSize(btnWidth, btnHeight);
         continueButton.setPosition(storyImage.getX() + (storyImage.getWidth() - btnWidth) / 2f, storyImage.getY() + paddingBottom);
